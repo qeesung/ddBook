df4e9fb1352b457a75750ddaea9d3d0bd9befcab
qeesung <1245712564@qq.com>
2015-01-22 04:15:38 +0800

    修复一些错误

--- sourceFiles/createNodeTree/createTree/createTree.cc
+++ sourceFiles/createNodeTree/createTree/createTree.cc
@@ -27,8 +27,8 @@ void addTreeNode(ddBook::NodeTree* nodeTree , const ddBook::Node & node)
 {
 		ddBook::Node * newNode = nodeTree->add_treenode();
 		newNode->set_startcode(node.startcode());
-		newNode->set_endcode(node->endcode());
-		newNode->set_previous(node->previous());
+		newNode->set_endcode(node.endcode());
+		newNode->set_previous(node.previous());
 		for (int i = 0; i < node.next_size(); ++i)
 		{
 			newNode->add_next(node.next(i));
