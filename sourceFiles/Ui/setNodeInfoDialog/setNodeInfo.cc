5a8b3668885605fd29a87a0532078961276a650a
qeesung <1245712564@qq.com>
2015-01-27 23:45:48 +0800

    子类化setNodeInfoDialog，并增加对应的信号与槽

---
+++ sourceFiles/Ui/setNodeInfoDialog/setNodeInfo.cc
@@ -0,0 +1,82 @@
+#include "setNodeInfo.h"
+#include <QFileDialog>
+
+SetNodeInfo::SetNodeInfo(QWidget * parent):QDialog(parent)
+{
+	setupUi(this);
+
+	defaultAudioListWidget->setSelectionMode(QAbstractItemView::MultiSelection);
+	connect(startAudioPushButton , SIGNAL(clicked()),\
+		    this , SLOT(startAudioView()));
+	connect(endAudioPushButton , SIGNAL(clicked()),\
+		    this , SLOT(endAudioView()));
+
+	connect(addPushButton, SIGNAL(clicked()),\
+		    this , SLOT(addDefaultAudio()));
+	connect(deletePushButton , SIGNAL(clicked()),\
+			this , SLOT(deleteDefaultAudio()));
+
+	connect(okPushButton , SIGNAL(clicked()),\
+		    this , SLOT(accept()));
+	connect(cancelPushButton , SIGNAL(clicked()),\
+		    this , SLOT(reject()));
+}
+
+void SetNodeInfo::startAudioView()
+{
+	QFileDialog * fileDialog = new QFileDialog;
+	fileDialog->setFileMode(QFileDialog::ExistingFile);
+	fileDialog->setNameFilter(QString("audio file(*.mp3 *.wmv)"));
+	fileDialog->setViewMode(QFileDialog::List);
+	fileDialog->setDirectory("../../res/");
+	fileDialog->setWindowTitle("Choose a start audio file");
+	if(fileDialog->exec()==QDialog::Accepted)
+	{
+		QString filename = fileDialog->selectedFiles()[0];
+		startAudioLineEdit->setText(filename);
+	}
+}
+
+void SetNodeInfo::endAudioView()
+{
+	QFileDialog * fileDialog = new QFileDialog;
+	fileDialog->setFileMode(QFileDialog::ExistingFile);
+	fileDialog->setNameFilter(QString("audio file(*.mp3 *.wmv)"));
+	fileDialog->setViewMode(QFileDialog::List);
+	fileDialog->setDirectory("../../res/");
+	fileDialog->setWindowTitle("Choose a end audio file");
+	if(fileDialog->exec()==QDialog::Accepted)
+	{
+		QString filename = fileDialog->selectedFiles()[0];
+		endAudioLineEdit->setText(filename);
+	}
+}
+
+void SetNodeInfo::addDefaultAudio()
+{
+	QFileDialog * fileDialog = new QFileDialog;
+	fileDialog->setFileMode(QFileDialog::ExistingFiles);
+	fileDialog->setNameFilter(QString("audio file(*.mp3 *.wmv)"));
+	fileDialog->setViewMode(QFileDialog::List);
+	fileDialog->setDirectory("../../res/");
+	fileDialog->setWindowTitle("Choose a default audio file");
+	if(fileDialog->exec()==QDialog::Accepted)
+	{
+		QStringList filenames = fileDialog->selectedFiles();
+		foreach(QString filename , filenames)
+		{
+			QListWidgetItem * newItem= new QListWidgetItem(filename);
+			defaultAudioListWidget->addItem(newItem);
+		}
+	}
+}
+
+void SetNodeInfo::deleteDefaultAudio()
+{
+	QList<QListWidgetItem * > selectedItems = defaultAudioListWidget->selectedItems();
+	foreach(QListWidgetItem * item , selectedItems)
+	{
+		defaultAudioListWidget->removeItemWidget(item);
+		delete item;
+	}
+}
---
+++ sourceFiles/Ui/setNodeInfoDialog/setNodeInfo.h
@@ -0,0 +1,23 @@
+#ifndef SETNODEINFO_H
+#define SETNODEINFO_H
+#include "setNodeInfoDialog.h"
+#include <QDialog>
+// #include "node.h"
+
+class SetNodeInfo : public QDialog , public Ui::setNodeInfoDialog
+{
+	Q_OBJECT
+public:
+	SetNodeInfo(QWidget * parent=0);
+	~SetNodeInfo(){}
+private slots:
+	void startAudioView();
+	void endAudioView();
+
+	void addDefaultAudio();
+	void deleteDefaultAudio();
+};
+
+
+
+#endif
