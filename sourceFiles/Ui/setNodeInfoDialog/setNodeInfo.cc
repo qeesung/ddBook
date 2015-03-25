8b487d793020d22a7c184eead46a95892ce79550
qeesung <1245712564@qq.com>
2015-03-25 18:55:46 +0800

    增加对节点其他信息的修改支持

--- sourceFiles/Ui/setNodeInfoDialog/setNodeInfo.cc
+++ sourceFiles/Ui/setNodeInfoDialog/setNodeInfo.cc
@@ -1,36 +1,109 @@
 #include "setNodeInfo.h"
+#include <QDebug>
 #include <QFileDialog>
 
 SetNodeInfo::SetNodeInfo(const Node * node,QWidget * parent):QDialog(parent)
 {
 	setupUi(this);
+	/** 使得表可以自由伸展 */
+	startAudioTableWidget->horizontalHeader()->setResizeMode(QHeaderView::Stretch);
+	endAudioTableWidget->horizontalHeader()->setResizeMode(QHeaderView::Stretch);
+	givenAudioTableWidget->horizontalHeader()->setResizeMode(QHeaderView::Stretch);
+	// startAudioTableWidget->verticalHeader()->setResizeMode(QHeaderView::Stretch);
+	// endAudioTableWidget->verticalHeader()->setResizeMode(QHeaderView::Stretch);
+	// givenAudioTableWidget->verticalHeader()->setResizeMode(QHeaderView::Stretch);
 
 	/** 在启动dialog时候自动载入节点信息 */
 	nodeNameLineEdit->setText(node->getText());
 	nodeNameLineEdit->selectAll();//选中全部的Node名字
 
+	transCodeLineEdit->setText(node->getTransCode());
+
+	pictureLineEdit->setText(node->getPicture());
+
 	startAudioLineEdit->setText(node->getStartAudio());
 	endAudioLineEdit->setText(node->getEndAudio());
+	
 	QStringList filenames = node->getDefaultAudioList();
 	foreach(QString filename , filenames)
 	{
 		QListWidgetItem * tempItem = new QListWidgetItem(filename);
-		defaultAudioListWidget->addItem(tempItem);
+		defaultAudiotListWidget->addItem(tempItem);
 	}
-	//将listWidget设置为多选选项
-	defaultAudioListWidget->setSelectionMode(QAbstractItemView::MultiSelection);
+
+	/** 得到start table的值 */
+	QMap<QString , QString> startAudioTableMap = node->getAllStartTableAudio();
+	unsigned int i = 0;
+	for(QMap<QString , QString>::const_iterator iter = startAudioTableMap.constBegin() ;\
+											   iter != startAudioTableMap.constEnd() ; ++iter , ++i)
+	{
+		QTableWidgetItem * item1 = new QTableWidgetItem(iter.key());
+		QTableWidgetItem * item2 = new QTableWidgetItem(iter.value());
+		startAudioTableWidget->insertRow(i);
+		startAudioTableWidget->setItem(i,0 ,item1);
+		startAudioTableWidget->setItem(i,1 ,item2);
+	}
+	/** 得到end table的值 */
+	QMap<QString , QString> endAudioTableMap = node->getAllEndTableAudio();
+	i=0;
+	for(QMap<QString , QString>::const_iterator iter = endAudioTableMap.constBegin() ;\
+											   iter != endAudioTableMap.constEnd() ; ++iter , ++i)
+	{
+		QTableWidgetItem * item1 = new QTableWidgetItem(iter.key());
+		QTableWidgetItem * item2 = new QTableWidgetItem(iter.value());
+		endAudioTableWidget->insertRow(i);
+		endAudioTableWidget->setItem(i,0 ,item1);
+		endAudioTableWidget->setItem(i,1 ,item2);
+	}
+	/** 得到given table的值 */
+	i=0;
+	QMap<QString , QString> givenAudioTableMap = node->getAllGivenTableAudio();
+	for(QMap<QString , QString>::const_iterator iter = givenAudioTableMap.constBegin() ;\
+											   iter != givenAudioTableMap.constEnd() ; ++iter , ++i)
+	{
+		QTableWidgetItem * item1 = new QTableWidgetItem(iter.key());
+		QTableWidgetItem * item2 = new QTableWidgetItem(iter.value());
+		givenAudioTableWidget->insertRow(i);
+		givenAudioTableWidget->setItem(i,0 ,item1);
+		givenAudioTableWidget->setItem(i,1 ,item2);
+	}
+
+	//将表设置为多选选项
+	defaultAudiotListWidget->setSelectionMode(QAbstractItemView::MultiSelection);
+	startAudioTableWidget->setSelectionMode(QAbstractItemView::MultiSelection);
+	endAudioTableWidget->setSelectionMode(QAbstractItemView::MultiSelection);
+	givenAudioTableWidget->setSelectionMode(QAbstractItemView::MultiSelection);
 	//ok按钮为默认的按钮
 	okPushButton->setDefault(true);
 
 	connect(startAudioPushButton , SIGNAL(clicked()),\
+
 		    this , SLOT(startAudioView()));
 	connect(endAudioPushButton , SIGNAL(clicked()),\
 		    this , SLOT(endAudioView()));
 
-	connect(addPushButton, SIGNAL(clicked()),\
+	connect(picturePushButton , SIGNAL(clicked()),\
+		    this , SLOT(pictureView()));
+
+	connect(defaultAudioAddPushButton, SIGNAL(clicked()),\
 		    this , SLOT(addDefaultAudio()));
-	connect(deletePushButton , SIGNAL(clicked()),\
+	connect(defaultAudioDeletePushButton , SIGNAL(clicked()),\
 			this , SLOT(deleteDefaultAudio()));
+
+	connect(startAudioAddPushButton, SIGNAL(clicked()),\
+		    this , SLOT(addStartTableAudio()));
+	connect(startAudioDeletePushButton , SIGNAL(clicked()),\
+			this , SLOT(deleteStartTableAudio()));
+
+	connect(endAudioAddPushButton, SIGNAL(clicked()),\
+		    this , SLOT(addEndTableAudio()));
+	connect(endAudioDeletePushButton , SIGNAL(clicked()),\
+			this , SLOT(deleteEndTableAudio()));
+
+	connect(givenAudioAddPushButton, SIGNAL(clicked()),\
+		    this , SLOT(addGivenTableAudio()));
+	connect(givenAudioDeletePushButton , SIGNAL(clicked()),\
+			this , SLOT(deleteGivenTableAudio()));
 
 	connect(okPushButton , SIGNAL(clicked()),\
 		    this , SLOT(accept()));
@@ -82,17 +155,103 @@ void SetNodeInfo::addDefaultAudio()
 		foreach(QString filename , filenames)
 		{
 			QListWidgetItem * newItem= new QListWidgetItem(filename);
-			defaultAudioListWidget->addItem(newItem);
+			defaultAudiotListWidget->addItem(newItem);
 		}
 	}
 }
 
 void SetNodeInfo::deleteDefaultAudio()
 {
-	QList<QListWidgetItem * > selectedItems = defaultAudioListWidget->selectedItems();
+	QList<QListWidgetItem * > selectedItems = defaultAudiotListWidget->selectedItems();
 	foreach(QListWidgetItem * item , selectedItems)
 	{
-		defaultAudioListWidget->removeItemWidget(item);
+		defaultAudiotListWidget->removeItemWidget(item);
 		delete item;
+	}
+}
+
+
+void SetNodeInfo::pictureView()
+{
+	QFileDialog * fileDialog = new QFileDialog;
+	fileDialog->setFileMode(QFileDialog::ExistingFile);
+	fileDialog->setNameFilter(QString("picture file(*.jpg *.png)"));
+	fileDialog->setViewMode(QFileDialog::List);
+	fileDialog->setDirectory("../../res/");
+	fileDialog->setWindowTitle("Choose a node picture");
+	if(fileDialog->exec()==QDialog::Accepted)
+	{
+		QString filename = fileDialog->selectedFiles()[0];
+		pictureLineEdit->setText(filename);
+	}
+}
+
+void SetNodeInfo::addStartTableAudio()
+{
+	GetTablePairDialog * dialog = new GetTablePairDialog;
+	dialog->setWindowTitle("FromCode --- AudioFile");
+	if(dialog->exec() == QDialog::Accepted)
+	{
+		QTableWidgetItem * item1 = new QTableWidgetItem(dialog->getCode());
+		QTableWidgetItem * item2 = new QTableWidgetItem(dialog->getFile());
+		unsigned int rowCount = startAudioTableWidget->rowCount();
+		startAudioTableWidget->insertRow(rowCount);
+		startAudioTableWidget->setItem(rowCount , 0 , item1);
+		startAudioTableWidget->setItem(rowCount , 1 , item2);
+	}
+}
+
+void SetNodeInfo::deleteStartTableAudio()
+{
+	QList<QTableWidgetItem *> selectedItems = startAudioTableWidget->selectedItems();
+	foreach(QTableWidgetItem * item , selectedItems)
+	{
+		startAudioTableWidget->removeRow(startAudioTableWidget->row(item));
+	}
+}
+
+void SetNodeInfo::addEndTableAudio()
+{
+	GetTablePairDialog * dialog = new GetTablePairDialog;
+	dialog->setWindowTitle("ToCode --- AudioFile");
+	if(dialog->exec() == QDialog::Accepted)
+	{
+		QTableWidgetItem * item1 = new QTableWidgetItem(dialog->getCode());
+		QTableWidgetItem * item2 = new QTableWidgetItem(dialog->getFile());
+		unsigned int rowCount = endAudioTableWidget->rowCount();
+		endAudioTableWidget->insertRow(rowCount);
+		endAudioTableWidget->setItem(rowCount , 0 , item1);
+		endAudioTableWidget->setItem(rowCount , 1 , item2);
+	}
+}
+void SetNodeInfo::deleteEndTableAudio()
+{
+	QList<QTableWidgetItem *> selectedItems = endAudioTableWidget->selectedItems();
+	foreach(QTableWidgetItem * item , selectedItems)
+	{
+		endAudioTableWidget->removeRow(endAudioTableWidget->row(item));
+	}
+}
+
+void SetNodeInfo::addGivenTableAudio()
+{
+	GetTablePairDialog * dialog = new GetTablePairDialog;
+	dialog->setWindowTitle("GivenCode --- AudioFile");
+	if(dialog->exec() == QDialog::Accepted)
+	{
+		QTableWidgetItem * item1 = new QTableWidgetItem(dialog->getCode());
+		QTableWidgetItem * item2 = new QTableWidgetItem(dialog->getFile());
+		unsigned int rowCount = givenAudioTableWidget->rowCount();
+		givenAudioTableWidget->insertRow(rowCount);
+		givenAudioTableWidget->setItem(rowCount , 0 , item1);
+		givenAudioTableWidget->setItem(rowCount , 1 , item2);
+	}
+}
+void SetNodeInfo::deleteGivenTableAudio()
+{
+	QList<QTableWidgetItem *> selectedItems = givenAudioTableWidget->selectedItems();
+	foreach(QTableWidgetItem * item , selectedItems)
+	{
+		givenAudioTableWidget->removeRow(givenAudioTableWidget->row(item));
 	}
 }
--- sourceFiles/Ui/setNodeInfoDialog/setNodeInfo.h
+++ sourceFiles/Ui/setNodeInfoDialog/setNodeInfo.h
@@ -2,6 +2,7 @@
 #define SETNODEINFO_H
 #include "setNodeInfoDialog.h"
 #include <QDialog>
+#include "getTablePair.h"
 #include "node.h"
 
 class SetNodeInfo : public QDialog , public Ui::setNodeInfoDialog
@@ -14,16 +15,70 @@ public:
 	QString getNodeName() const{return nodeNameLineEdit->text();}
 	QString getStartAudio() const{return startAudioLineEdit->text();}
 	QString getEndAudio() const {return endAudioLineEdit->text();}
+	QString getTransCode() const {return transCodeLineEdit->text();}
+	QString getPictureName() const{return pictureLineEdit->text();}
+	/**
+	 * 得到默认音乐列表
+	 * @return 返回列表
+	 */
 	QStringList getDefaultAudioList() const
 	{
-		int itemCount = defaultAudioListWidget->count();
+		int itemCount = defaultAudiotListWidget->count();
 		QStringList defaultList;
 		for (int i = 0; i < itemCount; ++i)
 		{
-			QListWidgetItem * temp = defaultAudioListWidget->item(i);
+			QListWidgetItem * temp = defaultAudiotListWidget->item(i);
 			defaultList<<temp->text();
 		}
 		return defaultList;
+	}
+
+	/**
+	 *  得到预设好的入口音乐
+	 */
+	QList<QPair<QString , QString> > getStartAudioTable() const
+	{
+		unsigned int startAudioPairCount = startAudioTableWidget->rowCount();
+		QList<QPair<QString , QString> > retList;
+		for (unsigned int i = 0; i < startAudioPairCount; ++i)
+		{
+			QTableWidgetItem * item1 = startAudioTableWidget->item(i,0);
+			QTableWidgetItem * item2 = startAudioTableWidget->item(i,1);
+			retList<<QPair<QString , QString>(item1->text() , item2->text());
+		}
+		return retList;
+	}
+
+	/**
+	 * 得到预设好的出口音乐
+	 */
+	QList<QPair<QString , QString> > getEndAudioTable() const
+	{
+		unsigned int endAudioPairCount = endAudioTableWidget->rowCount();
+		QList<QPair<QString , QString> > retList;
+		for (unsigned int i = 0; i < endAudioPairCount; ++i)
+		{
+			QTableWidgetItem * item1 = endAudioTableWidget->item(i,0);
+			QTableWidgetItem * item2 = endAudioTableWidget->item(i,1);
+			retList<<QPair<QString , QString>(item1->text() , item2->text());
+		}
+		return retList;
+	}
+
+	/**
+	 * 得到给定的码制对应默认音乐
+	 */
+	QList<QPair<QString , QString> > getGivenAudioTable() const
+	{
+		unsigned int givenAudioPairCount = givenAudioTableWidget->rowCount();
+		QList<QPair<QString , QString> > retList;
+		for (unsigned int i = 0; i < givenAudioPairCount; ++i)
+		{
+			QTableWidgetItem * item1 = givenAudioTableWidget->item(i,0);
+			QTableWidgetItem * item2 = givenAudioTableWidget->item(i,1);
+			retList<<QPair<QString , QString>(item1->text() , item2->text());
+		}
+		return retList;
 	}
 private slots:
 	void startAudioView();
@@ -31,6 +86,17 @@ private slots:
 
 	void addDefaultAudio();
 	void deleteDefaultAudio();
+
+	void pictureView();
+
+	void addStartTableAudio();
+	void deleteStartTableAudio();
+
+	void addEndTableAudio();
+	void deleteEndTableAudio();
+
+	void addGivenTableAudio();
+	void deleteGivenTableAudio();
 };
 
 
