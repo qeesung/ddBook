b26b6e8d99890a04a594b2d5e4e107999d6a2e50
qeesung <1245712564@qq.com>
2015-04-09 16:57:14 +0800

    在加载mainWidow以后加载chooseCpAlFileDialog来选择基础数据文件

--- sourceFiles/Ui/main.cc
+++ sourceFiles/Ui/main.cc
@@ -1,10 +1,14 @@
 #include <QApplication>
 #include "mainWindow.h"
+#include "chooseCpAlDialog.h"
 
 int main(int argc, char *argv[])
 {
 	QApplication app(argc, argv);
 	MainWindow * mainwindow = new MainWindow;
 	mainwindow->show();
+    /** 选择基础数据文件 */
+    ChooseCpAlDialog * cpAlDialog = new ChooseCpAlDialog();
+    cpAlDialog->exec();
 	return app.exec();
 }
