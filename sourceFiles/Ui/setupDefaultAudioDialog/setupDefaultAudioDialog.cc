cd956cf03f6d106bba21601f1e95062beb7e5d9c
qeesung <1245712564@qq.com>
2015-04-27 20:42:19 +0800

    在保存cd文件时自动更新dockWidget信息

--- sourceFiles/Ui/setupDefaultAudioDialog/setupDefaultAudioDialog.cc
+++ sourceFiles/Ui/setupDefaultAudioDialog/setupDefaultAudioDialog.cc
@@ -143,6 +143,7 @@ void SetupDefaultAudioDialog::saveDefaultAudio()
 	}
 	file.close();
 	MainWindow::loadCdData();
+	MainWindow::updateDockWidget();
 }
 
 void SetupDefaultAudioDialog::searchCode(const QString & code)
